8dabf43c0fd816f0b2d740004d4acb9a2afca3be
iamsixwater <soup.sixwater@gmail.com>
2023-10-23 16:10:31 +1100

    [ADD] saving location data

--- mern_client/src/apis/info.ts
+++ mern_client/src/apis/info.ts
@@ -3,3 +3,6 @@ import { Info } from '../types/info';
 
 export const getInfos = () =>
   axios.get<{ message: 'string'; data: Info[] }>('/api/infos');
+
+export const createInfo = (info: Info) =>
+  axios.post<Info, { message: 'string' }>('/api/infos', info);
--- mern_client/src/components/MarkersContainer.tsx
+++ mern_client/src/components/MarkersContainer.tsx
@@ -5,55 +5,67 @@ import { infosAtom, selectInfoAtom } from '../atoms/info';
 import { Info } from '../types/info';
 import Marker from './common/Marker';
 import InfoWindow from './common/InfoWindow';
+import { useMutation } from 'react-query';
+import { createInfo } from '../apis/info';
+import { AxiosError } from 'axios';
+import { HttpCode } from '../types/httpCode';
 
 interface MarkersContainerProps {
-    type?: 'home' | 'upload';
+  type?: 'home' | 'upload';
 }
 
-function MarkersContainer({type='home'}: MarkersContainerProps) {
-    const map = useAtomValue(mapAtom);
-    const infos = useAtomValue(infosAtom);
-    const [selectInfo, setSelectInfo] = useAtom(selectInfoAtom);
+function MarkersContainer({ type = 'home' }: MarkersContainerProps) {
+  const map = useAtomValue(mapAtom);
+  const infos = useAtomValue(infosAtom);
+  const [selectInfo, setSelectInfo] = useAtom(selectInfoAtom);
 
-    const onSubmit = useCallback(() => {
-        console.log('submit!');
-    }, []);
+  const { mutate } = useMutation(createInfo, {
+    onSuccess: () => alert('Successfully Uploaded'),
+    onError: (err: AxiosError) => {
+      const errStatus = err.response?.status;
+      if (errStatus === HttpCode.CONFLICT) alert('Duplicated Data');
+      else alert('Internal Server Error');
+    },
+  });
 
-    if(!map || !infos) return null;
+  const onSubmit = useCallback(() => {
+    if (!selectInfo) return;
+    mutate(selectInfo);
+  }, [mutate, selectInfo]);
 
-    return (
-        <>
-            {infos.map((info: Info) => (
-                <Marker
-                    key={info.id}
-                    map={map}
-                    position={info.position}
-                    content={'<div class="marker" />'}
-                    onClick={() => {
-                        setSelectInfo(info);
-                    }}
-                />
-            ))}
-            {
-                selectInfo && (
-                    <Marker
-                        key={selectInfo.id}
-                        map={map}
-                        position={selectInfo.position}
-                        content={'<div class="marker select" />'}
-                        onClick={() => {
-                            setSelectInfo(null);
-                        }}
-                    />
-                )
-            }
-            <InfoWindow
-                map={map}
-                selectInfo={selectInfo}
-                onSubmit={type==='upload' ? onSubmit : undefined}
-            />
-        </>
-    );
+  if (!map || !infos) return null;
+
+  return (
+    <>
+      {infos.map((info: Info) => (
+        <Marker
+          key={info.id}
+          map={map}
+          position={info.position}
+          content={'<div class="marker" />'}
+          onClick={() => {
+            setSelectInfo(info);
+          }}
+        />
+      ))}
+      {selectInfo && (
+        <Marker
+          key={selectInfo.id}
+          map={map}
+          position={selectInfo.position}
+          content={'<div class="marker select" />'}
+          onClick={() => {
+            setSelectInfo(null);
+          }}
+        />
+      )}
+      <InfoWindow
+        map={map}
+        selectInfo={selectInfo}
+        onSubmit={type === 'upload' ? onSubmit : undefined}
+      />
+    </>
+  );
 }
 
 export default MarkersContainer;
